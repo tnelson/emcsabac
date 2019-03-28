8f17049bf47e6478de1f5cf3e3a17be5c559c4b0
Tim Nelson <tbnelson@gmail.com>
2019-03-28 18:32:52 -0400

    deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,12 @@
 #lang info
 
+(define version "0.1")
+(define pkg-authors '(tn))
+
+(define deps '("base"
+               "rosette"
+               "ocelot"))
+
 (define name "emcsabac")
 ;(define scribblings '([]))
 (define collection "emcsabac")
