40ac972ced045296adc03c88bf1ce62e9f618922
Tim Nelson <tbnelson@gmail.com>
2019-03-28 18:17:58 -0400

    further cleanup

--- info.rkt
+++ info.rkt
@@ -3,4 +3,4 @@
 (define name "emcsabac")
 ;(define scribblings '([]))
 (define collection "emcsabac")
-;(define compile-omit-paths '("extra-files" "extended-examples"))
+(define compile-omit-paths '("test"))
